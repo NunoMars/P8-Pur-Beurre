e7c8c7b0e53ad49f43efc194d817517f63086749
NunoMars <nuno.ricardo.mars@gmail.com>
2020-08-27 09:54:31 +0200

    new readme

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'PurBeurre', # le nom de notre base de donnees creee precedemment
+        'USER': 'Remi', # attention : remplacez par votre nom d'utilisateur
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
