acd4b2cd99235afb8b9acd4e609dd5b59770ecd9
nuno <nuno.ricardo.mars@gmail.com>
2020-11-19 21:48:21 +0100

    try to collect static

--- purbeurreconfig/settings.py
+++ purbeurreconfig/settings.py
@@ -128,12 +128,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, "staticfiles"),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 django_heroku.settings(locals())
