d35cf59b7484131400b8d69691661c3dcb725bcd
nuno <nuno.ricardo.mars@gmail.com>
2020-11-19 16:29:48 +0100

    deplace tests jsons

--- products/tests_clear_data_dowload_products.py
+++ products/tests_clear_data_dowload_products.py
@@ -11,12 +11,12 @@ from .download_products import DataFiles
     
 class DownloadProducts(TestCase):
     def test_call_openfoodfacts(self):
-        with open("products_data.json") as products_data:
+        with open("for tests/products_data.json") as products_data:
             results_test = json.load(products_data)
 
         assert DataFiles.all_products == results_test
 
-        with open('products_data_eliminate_duplicated.json') as products_data_eliminated_products:
+        with open('for tests/products_data_eliminate_duplicated.json') as products_data_eliminated_products:
             results_test = json.load(products_data_eliminated_products)
 
         assert DataFiles.products_to_inser == results_test
