4d54eb2171965ff1be456f8e7df608d6246e994d
nuno <nuno.ricardo.mars@gmail.com>
2020-11-19 21:13:53 +0100

    done

--- purbeureconfig/wsgi.py
+++ purbeureconfig/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "purbeureconfig.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "purbeurreconfig.settings")
 
 application = get_wsgi_application()
