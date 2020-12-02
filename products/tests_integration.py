e9f687b8576a98def5a6a370965750004a7dae35
nuno <nuno.ricardo.mars@gmail.com>
2020-12-02 18:08:21 +0100

    work on celenium tests

--- products/tests_integration.py
+++ products/tests_integration.py
@@ -1,13 +1,31 @@
+from purbeurreconfig.settings import BASE_DIR
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('window-size=1920x1080')
+
+
 class TestProductSearch(unittest.TestCase):
 
-    def setUp(self):
-        self.driver = webdriver.Firefox(executable_path=r'C:/OPENCLASSROOMS/Projet8/webdrivers/geckodriver.exe')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = webdriver.Chrome(
+            executable_path=str(BASE_DIR / 'webdrivers' / 'chromedriver'),
+            options=chrome_options,
+            )
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.driver.quit()        
 
     def test_results_page_shows(self):
         browser = self.driver.get("https://nunopurbeurre.herokuapp.com/")
@@ -20,9 +38,6 @@ class TestProductSearch(unittest.TestCase):
 
         self.assertEqual(page_url, 'https://nunopurbeurre.herokuapp.com/products/3265261606676/')
         self.assertEqual(page_title, u'Page Products')
-
-    def tearDown(self):
-        self.driver.close()
 
 
 if __name__ == '__main__':
