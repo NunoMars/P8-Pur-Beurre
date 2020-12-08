5d416e2ba1884711bd5e746471e6b3964db58f12
nuno <nuno.ricardo.mars@gmail.com>
2020-12-09 00:10:02 +0100

    nose coverage 87%

--- purbeurreconfig/settings.py
+++ purbeurreconfig/settings.py
@@ -40,19 +40,19 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "products.apps.ProductsConfig",
     "accounts.apps.AccountsConfig",
-    #'django_nose',
+    'django_nose',
 ]
 
-"""
+
 # Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=foo,bar',
+    '--cover-package=products,accounts',
 ]
-"""
+
 ######################AUTH#########################
 
 AUTH_USER_MODEL = "accounts.CustomUser"
