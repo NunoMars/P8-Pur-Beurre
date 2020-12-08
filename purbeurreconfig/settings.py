d239d6a1b78f51a74c2ade044bdf06e387daed90
nuno <nuno.ricardo.mars@gmail.com>
2020-12-09 00:12:38 +0100

    nose out of tests

--- purbeurreconfig/settings.py
+++ purbeurreconfig/settings.py
@@ -40,18 +40,18 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "products.apps.ProductsConfig",
     "accounts.apps.AccountsConfig",
-    'django_nose',
+    #'django_nose',
 ]
 
 
-# Use nose to run all tests
+"""# Use nose to run all tests
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
     '--cover-package=products,accounts',
-]
+]"""
 
 ######################AUTH#########################
 
