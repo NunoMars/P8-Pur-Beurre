fea41deb039934df2d487f1ed930ca3ae571e37f
nuno <nuno.ricardo.mars@gmail.com>
2020-12-28 16:20:21 +0100

    new config

--- purbeurreconfig/settings/travis.py
+++ purbeurreconfig/settings/travis.py
@@ -8,18 +8,13 @@ TEMPLATE_DEBUG = True
 
 SECRET_KEY = '-~aO;| F;rE[??/w^zc4mh(9'
 
-INSTALLED_APPS = [
-    "products.apps.ProductsConfig",
-    "accounts.apps.AccountsConfig",
-]
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",  # on utilise l'adaptateur postgresql
         "NAME": "purbeurre",  # le nom de notre base de donnees creee precedemment
         "USER": "postgres",  # attention : remplacez par votre nom d'utilisateur
         "PASSWORD": "",
-        "HOST": "",
-        "PORT": "127.0.0.1",
+        "HOST": "127.0.0.1",
+        "PORT": "",
     }
 }
