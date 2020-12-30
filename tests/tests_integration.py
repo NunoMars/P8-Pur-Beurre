707620be67eb464af36fae7e92e43d4d48b82ac2
nuno <nuno.ricardo.mars@gmail.com>
2020-12-30 19:32:57 +0100

    Travis config chromedriver

--- tests/tests_integration.py
+++ tests/tests_integration.py
@@ -24,6 +24,7 @@ class TestIntegrations(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.driver = webdriver.Chrome(
+            executable_path=str(BASE_DIR / 'chromedriver'),
             options=chrome_options,
             )
         cls.driver.maximize_window()    
